1e7e4e0f1d5fc43bafe43563ac49db6912e1d931
reggi <thomas@reggi.com>
2014-03-14 12:41:12 -0400

    require jquery

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 console.log('loading main');
 var bromote = require('bromote');
-bromote.jquery(function($) {
-    console.log('jquery version: ', $().jquery);
-    $(document).ready(function() {
-        bromote.google(function(google) {
-            var map = new google.maps.Map(document.getElementById('map-canvas'), {
-                zoom: 11,
-                center: new google.maps.LatLng(40.670287895547006, -73.9865255355835)
-            });
+var jquery = require("jquery");
+
+$(document).ready(function() {
+    bromote.google(function(google) {
+        var map = new google.maps.Map(document.getElementById('map-canvas'), {
+            zoom: 11,
+            center: new google.maps.LatLng(40.670287895547006, -73.9865255355835)
         });
     });
 });
