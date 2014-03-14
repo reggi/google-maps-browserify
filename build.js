c87a6f28ae9d3b93e58d3134f70a787e8c9fb3fa
reggi <thomas@reggi.com>
2014-03-14 12:35:38 -0400

    inital orphan branch

---
+++ build.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var browserify = require('browserify');
+var shim = require('browserify-shim');
+var bromote = require('bromote');
+var bconfig = require('bconfig');
+var PassThrough = require('stream').PassThrough;
+
+var fs = require('fs');
+var path = require('path');
+
+var config = bconfig(require.resolve('./requirejs-config'));
+
+function build(debug) {
+
+    var passThrough = new PassThrough();
+
+    var bify = shim(browserify(), config.shim);
+
+    bromote(bify, config.remote, function(err, gens) {
+        if (err) return console.error(err);
+
+        bify
+            .require(config.entry, {
+                entry: true
+            })
+            .bundle({
+                debug: debug
+            })
+            .pipe(passThrough);
+    });
+    return passThrough;
+}
+
+var bundlePath = path.join(__dirname, 'public', 'bundle.js');
+build(true).pipe(fs.createWriteStream(bundlePath, 'utf8'));
